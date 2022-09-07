bbdb7fc192d5ac07c54fa545c5e940eddc284dbc
Vladimirrom <vladimirrom0707@gmail.com>
2022-09-07 22:37:59 +0300

    created repositories for User, Role and Product entities

---
+++ src/main/java/com/mycompany/onlineshop/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.mycompany.onlineshop.repositories;
+
+import com.mycompany.onlineshop.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+}
