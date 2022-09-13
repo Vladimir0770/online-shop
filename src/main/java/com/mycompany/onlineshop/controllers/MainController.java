bee2af3c46801f713c97e8228c1aca37a3eb84c9
Vladimirrom <vladimirrom0707@gmail.com>
2022-09-13 12:38:16 +0300

    added design to index.html

--- src/main/java/com/mycompany/onlineshop/controllers/MainController.java
+++ src/main/java/com/mycompany/onlineshop/controllers/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String homePage() { return "home";}
+    public String homePage() { return "home_page/index";}
 
     @GetMapping("/profile")
     public String accountPage() {return "profile";}
