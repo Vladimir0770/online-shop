4f870f125e456f5fcfaf4416c6a953b9c8dc17f5
Vladimirrom <vladimirrom0707@gmail.com>
2022-10-03 22:20:32 +0300

    created profile for user

--- src/main/java/com/mycompany/onlineshop/controllers/MainController.java
+++ src/main/java/com/mycompany/onlineshop/controllers/MainController.java
@@ -1,16 +1,33 @@
 package com.mycompany.onlineshop.controllers;
 
+import com.mycompany.onlineshop.entities.User;
+import com.mycompany.onlineshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class MainController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String homePage() { return "home_page/index";}
 
     @GetMapping("/profile")
-    public String accountPage() {return "profile";}
+    public String accountPage(Principal principal, Model model) {
+        User user = userService.findByUsername(principal.getName());
+        model.addAttribute("currentUser", user);
+        return "profile";
+    }
 
     @GetMapping("/admins")
     public String adminsPage() {return "admins";}
